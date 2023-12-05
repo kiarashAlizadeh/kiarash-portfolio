7e1709476aa110210db40ea2f11e60386de6c755
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-12-05 16:10:58 +0330

    initial project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <Footer />
+    </>
+  )
+}
+
+export default Home
