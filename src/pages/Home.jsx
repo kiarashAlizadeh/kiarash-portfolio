3ac74d0c3a7b40563b9ec4f2ec2da050dd8dfad3
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-12-05 23:34:37 +0330

    home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Footer from "../components/Footer"
+import Header from "../components/Header"
 import Navbar from "../components/Navbar"
+import Projects from "../components/Projects"
 
 function Home() {
   return (
     <>
       <Navbar />
-
+      <Header />
+      <Projects />
       <Footer />
     </>
   )
